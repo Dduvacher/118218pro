a203b17593f5379c13b70602a7eca365727bdfcb
Damien Duvacher <damien.duvacher@calldesk.fr>
2020-02-07 16:55:47 +0100

    Add test file example

---
+++ test/societe-info.utils.spec.ts
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const rewire = require('rewire');
+
+describe('societe-info', () => {
+
+describe('getSocieteInfo', () => {
+    afterEach(() => {
+      sinon.restore();
+      return Promise.resolve();
+    });
+
+    it('should ...', () => {
+    });
+
+    it('should ...', () => {
+
+    });
+  });
+
+  describe('someFunction', () => {
+    afterEach(() => {
+      sinon.restore();
+      return Promise.resolve();
+    });
+
+    it('should ...', () => {
+
+    });
+  });
+
+});
